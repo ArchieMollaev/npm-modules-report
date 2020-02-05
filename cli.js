69037d14678aa38aeb5740b4735c9ff7982eb44c
Artur Mollaev <artur.mollaev@yojji.io>
2020-02-05 22:17:04 +0200

    fix cli

--- cli.js
+++ cli.js
@@ -2,10 +2,8 @@
 
 const [, , ...args] = process.argv;
 
-const { getPackagesReport } = require("./index");
+const generateReport = require("./index");
 const package = require("../../package.json");
 const columns = args.length ? args : ["name", "description"];
 
-getPackagesReport(package, columns).then(output => {
-  console.log("output", output);
-});
+generateReport(package, columns);
